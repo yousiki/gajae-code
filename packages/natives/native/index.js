169dd73a4ba7798d7bd05289ac5cf2561cbf8c50
YouSiki <you.siki@outlook.com>
2026-07-09 00:04:56 +0900

    chore: bump version to 0.9.2

--- packages/natives/native/index.js
+++ packages/natives/native/index.js
@@ -27,7 +27,7 @@ export const PtySession = nativeBindings.PtySession;
 export const Shell = nativeBindings.Shell;
 
 // functions
-export const __piNativesV0_9_1 = nativeBindings.__piNativesV0_9_1;
+export const __piNativesV0_9_2 = nativeBindings.__piNativesV0_9_2;
 export const applyBashFixups = nativeBindings.applyBashFixups;
 export const astEdit = nativeBindings.astEdit;
 export const astGrep = nativeBindings.astGrep;
